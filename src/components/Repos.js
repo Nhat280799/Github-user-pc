c5ef23d99ec034728657a78ebd53a5a9014796ef
DESKTOP-SO6C8LR\HP <hoatamdv23@gmail.com>
2022-05-10 17:13:19 +0700

    update chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,6 +5,23 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const {repos} = React.useContext(GithubContext);
   console.log(repos);
+  let languages = repos.reduce((total,item) => {
+      const {language} = item;
+      if(!language){
+          return total;
+      }
+      if(!total[language]){
+        total[language] = {label : language , value : 1 };
+      }else{
+        total[language] = {...total[language] , value: total[language].value + 1,};
+      }
+      return total;
+  },{});
+
+    languages = Object.values(languages).sort((a,b) => {
+      return b.value - a.value;
+    }).slice(0,5);
+
   const chartData = [
     {
       label: "HTML",
@@ -22,7 +39,9 @@ const Repos = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
-      <ExampleChart data={chartData}/>;
+      <Pie3D data={languages} />
+      <div></div>
+      <Doughnut2D data={chartData} />
       </Wrapper>
     </section>);
 };
